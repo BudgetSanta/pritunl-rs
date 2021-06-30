37b92a71a347453f8026e7a530bb2c2db347d109
Jared Steiner <steiner.jared1996@gmail.com>
2021-06-30 21:16:17 +1000

    Add broken events method

--- src/service/events.rs
+++ src/service/events.rs
@@ -1,5 +1,13 @@
-//impl Client {
-//    pub fn get_events(&self) -> Result<String, io::Error> {
-//        socket::get(self, "/events")
-//    }
-//}
+use crate::{socket, Client, Result};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Event {}
+
+impl Client {
+    pub fn get_events(&self) -> Result<Vec<Event>> {
+        let res = socket::get(self, "/events")?;
+        println!("{}\n{}", &res.headers, &res.body);
+        Ok(serde_json::from_str(&res.body)?)
+    }
+}
