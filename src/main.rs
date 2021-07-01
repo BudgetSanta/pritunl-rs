c0bc89e3c99e395055feb6d1bb347d02d2749694
Jared Steiner <steiner.jared1996@gmail.com>
2021-07-01 17:25:08 +1000

    Add pretty print method for checking profile statuses

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
+use core::time;
 use pritunl_rs::Client;
-use std::error::Error;
+use std::{error::Error, thread};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let r = Client::new();
 
-    println!("PROFILE: {:#?}", r.query_active_profiles()?);
-    println!("PROFILE: {:#?}", r.query_active_profiles()?);
-    println!("PROFILE: {:#?}", r.query_active_profiles()?);
+    let delay = time::Duration::from_secs(2);
 
+    loop {
+        print_server_status(&r)?;
+        println!("sleeping...");
+        thread::sleep(delay);
+    }
+
+    Ok(())
+}
+
+fn print_server_status(r: &Client) -> Result<(), Box<dyn Error>> {
+    let mut sprofiles = r.get_system_profiles()?;
+    let active_profiles = r.query_active_profiles()?;
+    sprofiles.sort_by(|a, b| a.server.cmp(&b.server));
+    for p in sprofiles {
+        let status = match active_profiles.contains_key(&p.id) {
+            true => match active_profiles.get(&p.id).unwrap().timestamp {
+                0 => "Connecting  ",
+                _ => "Connected   ",
+            },
+            false => "Disconnected",
+        };
+        println!("{} [{}] - {}", &p.id[..8], status, p.server);
+    }
     Ok(())
 }
