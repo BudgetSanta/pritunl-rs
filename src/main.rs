34f372e8fe4f5e25015164e60f6429aab0f25378
Jared Steiner <steiner.jared1996@gmail.com>
2021-07-02 14:02:20 +1000

    Provide pretty print for all profiles

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use core::time;
 use pritunl_rs::Client;
-use std::{error::Error, thread};
+use std::{
+    error::Error,
+    thread,
+    time::{Duration, SystemTime},
+    vec,
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let r = Client::new();
@@ -20,15 +25,31 @@ fn print_server_status(r: &Client) -> Result<(), Box<dyn Error>> {
     let mut sprofiles = r.get_system_profiles()?;
     let active_profiles = r.query_active_profiles()?;
     sprofiles.sort_by(|a, b| a.server.cmp(&b.server));
-    for p in sprofiles {
-        let status = match active_profiles.contains_key(&p.id) {
-            true => match active_profiles.get(&p.id).unwrap().timestamp {
-                0 => "Connecting  ",
-                _ => "Connected   ",
+    for sys_prof in sprofiles {
+        let profile = active_profiles.get(&sys_prof.id);
+        let status = match profile {
+            None => "Disconnected".into(),
+            Some(p) => match p.timestamp {
+                0 => "Connecting".into(),
+                _ => format!(
+                    "Up for ({})",
+                    format_time(p.timestamp)
+                        .to_string()
+                        .split_ascii_whitespace()
+                        .next()
+                        .unwrap()
+                ),
             },
-            false => "Disconnected",
         };
-        println!("{} [{}] - {}", &p.id[..8], status, p.server);
+        println!("{} [{}] - {}", &sys_prof.id[..8], status, sys_prof.server,);
     }
     Ok(())
+}
+
+fn format_time(timestamp: i64) -> humantime::FormattedDuration {
+    let duration = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        - Duration::new(timestamp as u64, 0);
+    humantime::format_duration(duration)
 }
